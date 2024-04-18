9ebab7380331fd7eb434bc588c44a65639742b67
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-04-19 00:35:17 +0500

    new style for signin/signup

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,13 +1,16 @@
 import { useSignUp } from "../hooks/useSignUp"
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 function Signin() {
   const {signupWithGoogle , user , error} = useSignUp()
   return (
     <div className="min-h-screen grid place-items-center">
+      <h2 className="font-bold text-4xl">Signin</h2>
     <button onClick={signupWithGoogle} className="btn btn-secondary">
     <FcGoogle className="text-3xl"/>
       <span className="text-2xl">Google</span></button>
+      <p><Link className="hover:text-violet-600" to="/signup" >Don't you have an account?</Link></p>
   </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import { useSignUp } from "../hooks/useSignUp"
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 
 
@@ -8,9 +9,12 @@ function Signup() {
 
     return (
       <div className="min-h-screen grid place-items-center">
+        <h2 className="font-bold text-4xl">Signup</h2>
         <button onClick={signupWithGoogle} className="btn btn-secondary">
         <FcGoogle className="text-3xl"/>
           <span className="text-2xl">Google</span></button>
+          <p><Link className="hover:text-violet-600" to="/signin">Are you already registered?</Link></p>
+          
       </div>
     )
   }
